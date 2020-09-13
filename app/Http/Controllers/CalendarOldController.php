bfde2355d6940b8413e8d5a8e010542865534aff
yuuki1218 <58942798+yuuki1218@users.noreply.github.com>
2020-09-13 12:53:06 +0900

    update composer

--- app/Http/Controllers/CalendarOldController.php
+++ app/Http/Controllers/CalendarOldController.php
@@ -7,30 +7,28 @@ use App\Calendar\CalendarView;
 use App\Models\Record;
 use App\Models\Calendar;
 
-class CalendarController extends Controller
-{
-    public function index(Request $request)
-    {
-        $date = $request->input("date");
-        if ($date && preg_match("/^[0-9]{4}-[0-9]{2}$/", $date)) {
-            $date = strtotime($date . "+1 day");
-        } else {
-            $date = null;
-        }
+// class CalendarController extends Controller
+// {
+//     public function index(Request $request)
+//     {
+//         $date = $request->input("date");
+//         if ($date && preg_match("/^[0-9]{4}-[0-9]{2}$/", $date)) {
+//             $date = strtotime($date . "+1 day");
+//         } else {
+//             $date = null;
+//         }
 
-        if (!$date) {
-            $date = time();
-        }
-        $records = Record::all();
-        $calendar = new CalendarView($records, $date);
-        $tag = $calendar->showCalendarTag($request->month, $request->year);
-        return view('admin.calendar.index', ['calendar_tag' => $tag, 'calendar' => $calendar]);
-    }
+//         if (!$date) {
+//             $date = time();
+//         }
+//         $records = Record::all();
+//         $calendar = new CalendarView($records, $date);
+//         $tag = $calendar->showCalendarTag($request->month, $request->year);
+//         return view('admin.calendar.index', ['calendar_tag' => $tag, 'calendar' => $calendar]);
+//     }
 
-    public function create()
-    {
-        return view('admin.calendar.create');
-    }
-
-    public function store()
-}
+    // public function create()
+    // {
+    //     return view('admin.calendar.create');
+    // }
+// }
--- public/js/app.js
+++ public/js/app.js
@@ -37313,6 +37313,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/styles/calendarstyles.scss":
+/*!***************************************************!*\
+  !*** ./resources/sass/styles/calendarstyles.scss ***!
+  \***************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ "./resources/sass/styles/styles.scss":
 /*!*******************************************!*\
   !*** ./resources/sass/styles/styles.scss ***!
@@ -37325,14 +37336,15 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /***/ }),
 
 /***/ 0:
-/*!***********************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/styles/styles.scss ***!
-  \***********************************************************************/
+/*!*******************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/styles/styles.scss ./resources/sass/styles/calendarstyles.scss ***!
+  \*******************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! C:\Users\Owner\Documents\PHP Project\Exercise\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Owner\Documents\PHP Project\Exercise\resources\sass\styles\styles.scss */"./resources/sass/styles/styles.scss");
+__webpack_require__(/*! C:\Users\Owner\Documents\PHP Project\Exercise\resources\sass\styles\styles.scss */"./resources/sass/styles/styles.scss");
+module.exports = __webpack_require__(/*! C:\Users\Owner\Documents\PHP Project\Exercise\resources\sass\styles\calendarstyles.scss */"./resources/sass/styles/calendarstyles.scss");
 
 
 /***/ })
